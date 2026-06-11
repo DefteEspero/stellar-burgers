f20ca274412f0a149394d84f403b17e08a29eba5
DefteEspero <defterespero@gmail.com>
2026-06-11 23:09:45 +0400

    Исправление

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -11,48 +11,51 @@ import { NavLink, useLocation } from 'react-router-dom';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const { pathname } = useLocation();
+  const isConstructorActive =
+    pathname === '/' || pathname.startsWith('/ingredients');
+  const isFeedActive = pathname.startsWith('/feed');
+  const isProfileActive = pathname.startsWith('/profile');
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
           <>
-            <BurgerIcon type={'primary'} />
+            <BurgerIcon type={isConstructorActive ? 'primary' : 'secondary'} />
             <NavLink
               to='/'
               end
-              className={({ isActive }) =>
-                `text ${
-                  isActive || pathname.startsWith('/ingredients')
-                    ? styles.link_active
-                    : styles.link
-                }`
+              className={() =>
+                `text ${isConstructorActive ? styles.link_active : styles.link}`
               }
             >
               Конструктор
             </NavLink>
           </>
+
           <>
-            <ListIcon type={'primary'} />
+            <ListIcon type={isFeedActive ? 'primary' : 'secondary'} />
             <NavLink
               to='/feed'
-              className={({ isActive }) =>
-                `text ${isActive ? styles.link_active : styles.link}`
+              className={() =>
+                `text ${isFeedActive ? styles.link_active : styles.link}`
               }
             >
               Лента заказов
             </NavLink>
           </>
         </div>
+
         <div className={styles.logo}>
           <Logo className='' />
         </div>
+
         <div className={styles.link_position_last}>
-          <ProfileIcon type={'primary'} />
+          <ProfileIcon type={isProfileActive ? 'primary' : 'secondary'} />
           <NavLink
             to='/profile'
-            className={({ isActive }) =>
-              `text ${isActive ? styles.link_active : styles.link}`
+            className={() =>
+              `text ${isProfileActive ? styles.link_active : styles.link}`
             }
           >
             {userName || 'Личный кабинет'}
